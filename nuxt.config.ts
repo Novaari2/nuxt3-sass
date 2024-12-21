8ba5adc846baf400ba7455fa0c0e0d135600cdf1
novaari2 <novaari20@gmail.com>
2024-12-22 01:39:09 +0700

    setup project and create auth github and goole with supabase

---
+++ nuxt.config.ts
@@ -0,0 +1,29 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss','@nuxtjs/supabase'],
+  components: [
+    {
+      path: '~/components/ui',
+      // this is required else Nuxt will autoImport '.ts' file
+      extensions: ['.vue'],
+      // prefix for your components e.g. UiButton
+      prefix: ''
+    },
+    {
+      path: '~/components',
+      // this is required else Nuxt will autoImport '.ts' file
+      extensions: ['.vue'],
+      // prefix for your components e.g. UiButton
+      prefix: ''
+    }
+  ],
+  supabase: {
+    redirectOptions: {
+      login: '/auth',
+      callback: '/confirm',
+      exclude: ['/'],
+    }
+  }
+})
